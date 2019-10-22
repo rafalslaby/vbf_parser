6bd4a0156722f6a68e9d2dbc3eae4acd25ddbd0f
Rafal Slaby <rafal.slaby222@gmail.com>
2019-10-22 21:54:45 +0200

    Fix finding header start

--- vbf_parser/header_extractor.py
+++ vbf_parser/header_extractor.py
@@ -1,11 +1,35 @@
 import io
 import re
-from typing import TextIO
+from typing import Pattern, TextIO, Union
+
+
+def _read_until(fp: TextIO, pattern: Union[Pattern, str]) -> bool:
+    """
+    >>> file = io.StringIO("abc; header {spanish inquisition")
+    >>> _read_until(file, r"header\s*{")
+    True
+    >>> file.read()
+    'spanish inquisition'
+
+    """
+    text = ""
+    while not re.search(pattern, text):
+        c = fp.read(1)
+        if not c:
+            break
+        text += c
+    else:
+        return True
+    return False
 
 
 def extract_header_body(fp: TextIO) -> str:
     """
     >>> header = 'trash;\\n header{\\n x=10;\\n} trash'
+    >>> extract_header_body(io.StringIO(header)).strip()
+    'x=10;'
+
+    >>> header = 'trash; header{ x=10;} trash'
     >>> extract_header_body(io.StringIO(header)).strip()
     'x=10;'
 
@@ -18,10 +42,7 @@ def extract_header_body(fp: TextIO) -> str:
     'x = 10; z = {1,{2,3}};'
 
     """
-    line = ""
-    # TODO: won't work without newline after "header {"
-    while not re.search(r"header\s*{", line):
-        line = fp.readline()
+    _read_until(fp, r"header\s*{")
     nested_level = 1
     header = []
     is_in_quotes = False
