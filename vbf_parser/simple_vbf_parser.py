c1ac05c2fb5b1daea7fd6ef2c230cb6abf5a40f2
Rafal Slaby <rafal.slaby222@gmail.com>
2019-10-23 18:50:07 +0200

    Add comment support to jsonify

--- vbf_parser/simple_vbf_parser.py
+++ vbf_parser/simple_vbf_parser.py
@@ -1,15 +1,36 @@
-import json
 import re
 
 VBF_TO_JSON_REPLACEMENTS = {";": ",", "{": "[", "}": "]", "=": ":"}
 
 
-def _fix_missing_quotes(vbf_string: str) -> str:
-    return re.sub(r"([^\s={};,]+)", r'"\1"', vbf_string)
+def _fix_missing_quotes(string: str) -> str:
+    """
+    >>> _fix_missing_quotes('identifier = value')
+    '"identifier" = "value"'
+    """
+    return re.sub(r"([^\s={};,]+)", r'"\1"', string)
+
+
+def _remove_asterisk_comments(string: str) -> str:
+    """
+    >>> _remove_asterisk_comments('before/* a\\nb\\nc*/after')
+    'beforeafter'
+    """
+    return re.sub(r"/\*[^\\]*\*/", "", string)
+
+
+def _remove_slash_comments(string: str) -> str:
+    """
+    >>> _remove_slash_comments('before// comment\\nafter')
+    'before\\nafter'
+    """
+    return re.sub(r"//.*", "", string)
 
 
 def _jsonify_vbf_part(vbf_string: str) -> str:
-    jsonified = _fix_missing_quotes(vbf_string)
+    jsonified = _remove_asterisk_comments(vbf_string)
+    jsonified = _remove_slash_comments(jsonified)
+    jsonified = _fix_missing_quotes(jsonified)
     for source, replacement in VBF_TO_JSON_REPLACEMENTS.items():
         jsonified = jsonified.replace(source, replacement)
     return jsonified
@@ -23,9 +44,8 @@ def _iter_quoted_strings(str_: str):
 def jsonify_vbf_header(header: str) -> str:
     """
     >>> from json import loads
-    >>> loads(jsonify_vbf_header('abc = 10; z = "a b"; y = {1,{2,3}  };'))
+    >>> loads(jsonify_vbf_header('abc = 10; //comment \\n z = "a b"; /* multi\\nline*/ y = {1,{2,3}  };'))
     {'abc': '10', 'z': 'a b', 'y': ['1', ['2', '3']]}
-
     """
     json_string = "{"
     last_quote_end_index = 0
