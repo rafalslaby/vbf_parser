b35e14f028707311e7baad04c553762aaa27e68c
Rafal Slaby <rafal.slaby222@gmail.com>
2019-10-28 21:41:37 +0100

    Switched to flit package creator

--- vbf_parser/__init__.py
+++ vbf_parser/__init__.py
@@ -1,3 +1,8 @@
+"""
+Simple vbf parser
+"""
+__version__ = "1.3.0"
+
 from .header_extractor import extract_header_body
 from .vbf_jsonifier import jsonify_vbf_header
 from .vbf_parser import lex_vbf_header, parse_vbf_tokens
