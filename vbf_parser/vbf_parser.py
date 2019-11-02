d9118e44b97456a8b016e1058e27877f6a7356a2
Rafal Slaby <rafal.slaby222@gmail.com>
2019-11-02 10:02:01 +0100

    Add comment support to lexer

--- vbf_parser/vbf_parser.py
+++ vbf_parser/vbf_parser.py
@@ -93,7 +93,32 @@ def lex_whitespace(header: str) -> Tuple[str, str]:
     return "", header[1:]
 
 
-# TODO: add comment support
+def lex_single_line_comment(header: str) -> Tuple[str, str]:
+    """
+    >>> lex_single_line_comment("a=10")
+    ('', 'a=10')
+    >>> lex_single_line_comment("//comment\\nb=20")
+    ('', 'b=20')
+    """
+    if header[:2] != "//":
+        return "", header
+    line_end_pos = header.find("\n")
+    return "", header[line_end_pos + 1 :]
+
+
+def lex_multi_line_comment(header: str) -> Tuple[str, str]:
+    """
+    >>> lex_multi_line_comment("a=10")
+    ('', 'a=10')
+    >>> lex_multi_line_comment("/*multiline\\ncomment*/b=20")
+    ('', 'b=20')
+    """
+    if header[:2] != "/*":
+        return "", header
+    end_comment_pos = header.find("*/")
+    return "", header[end_comment_pos + 2 :]
+
+
 def lex_vbf_header(header: str) -> List[str]:
     """
     >>> lex_vbf_header('x = "ab"')
@@ -102,9 +127,18 @@ def lex_vbf_header(header: str) -> List[str]:
     ['y', '=', '{', '1', ',', '2', ',', '3', '}']
     >>> lex_vbf_header('y={{1,2},3};  abc=   0x12;')
     ['y', '=', '{', '{', '1', ',', '2', '}', ',', '3', '}', ';', 'abc', '=', '0x12', ';']
+    >>> lex_vbf_header('x=1; //comment \\ny=2; /* multiline \\n comment */z=3')
+    ['x', '=', '1', ';', 'y', '=', '2', ';', 'z', '=', '3']
     """
     tokens = []
-    lexing_functions = [lex_quoted, lex_unquoted_value, lex_syntax, lex_whitespace]
+    lexing_functions = [
+        lex_quoted,
+        lex_unquoted_value,
+        lex_syntax,
+        lex_whitespace,
+        lex_single_line_comment,
+        lex_multi_line_comment,
+    ]
     while header:
         for lex_func in lexing_functions:
             token, header = lex_func(header)
@@ -147,6 +181,10 @@ def parse_vbf_tokens(tokens: List[str]):
     >>> parse_vbf_tokens(tokens)
     {'x': 255}
 
+    >>> tokens = lex_vbf_header('abc = 0xff; //comment \\n z = "a b"; /* multi\\nline*/ y = {1,{2,3}  };')
+    >>> parse_vbf_tokens(tokens)
+    {'abc': 255, 'z': '"a b"', 'y': ['1', ['2', '3']]}
+     # TODO: fix quoted parsing
     """
     result = {}
     value: Union[int, list, str]
